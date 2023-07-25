303a53132523ee150e1eff4b19488b8c89c824ed
Shakira <shakira8046@gmail.com>
2023-07-25 15:04:51 +0200

    update

--- pkg/edge/corewebview2.go
+++ pkg/edge/corewebview2.go
@@ -8,9 +8,9 @@ import (
 	"runtime"
 	"unsafe"
 
-	"github.com/jchv/go-webview2/internal/w32"
+	"github.com/bdsoftpro/webview/internal/w32"
 
-	"github.com/jchv/go-webview2/webviewloader"
+	"github.com/bdsoftpro/webview/webviewloader"
 	"golang.org/x/sys/windows"
 )
 
